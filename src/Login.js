b8508e6022e70d9594f5d31c0d904c23bb744c76
Javier Gamarra <nhpatt@gmail.com>
2016-11-28 10:48:48 +0100

    play with template

--- src/Login.js
+++ src/Login.js
@@ -3,9 +3,31 @@
 import templates from './Login.soy.js';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
+import dom from 'metal-dom';
 
 class Login extends Component {
+
+	created() {
+	}
+
+	attached() {
+	}
+
+	login() {
+		console.log('a');
+		event.stopPropagation();
+	}
+
 }
+
+Login.STATE = {
+
+	passwordElement: {
+		setter: dom.toElement
+	},
+
+};
+
 Soy.register(Login, templates);
 
 export default Login;
