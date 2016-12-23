1c8435b490f00359eb21fe19c55e6686d24f4f8c
Julien C <julien.castelain@liferay.com>
2016-12-23 09:27:12 +0100

    Add vulcanize and crisper to experiment with webcomponents output

--- tasks/index.js
+++ tasks/index.js
@@ -1,12 +1,30 @@
 'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+var crisper = require('gulp-crisper');
 
 var checkOptions = require('./options');
 var webComponents = require('./webcomponents');
 
 module.exports.register = function(options) {
 	var opts = checkOptions(options);
+
+	// This task separates the HTML and JS from the webcomponent file
+	// for the moment, it's just defined to see what output we get.
+	gulp.task('vulcanize', ['webcomponents'], function (cb) {
+		gulp.src(path.join(opts.output, '*.html'))
+			.pipe(vulcanize({
+				inlineCss: true,
+				inlineSripts: true,
+				scriptComments: true
+			}))
+			.pipe(crisper())
+			.pipe(gulp.dest(path.join(opts.output, 'elements')))
+			.on('end', function () { cb(); })
+			.on('error', function (err) { cb(err); });
+	});
 
 	gulp.task('webcomponents', function (cb) {
 		gulp.src(opts.src)
